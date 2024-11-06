a6248121a7e7519336e005ed59ea4ba4d5a92282
OlamideID <iakinola926@gmail.com>
2024-11-06 10:28:20 +0100

    "Added device_preview package and integrated it into main.dart, updated pubspec.lock and pubspec.yaml accordingly."

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -31,7 +32,6 @@ final darkTheme = ThemeData(
   textTheme: GoogleFonts.latoTextTheme(Typography.whiteCupertino),
 );
 
-
 void main() {
   runApp(
     const ProviderScope(
@@ -45,12 +45,39 @@ class App extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-     final themeMode = ref.watch(themeNotifierProvider);
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: theme,
-        darkTheme: darkTheme,
-        themeMode: themeMode,
-        home: const TabsScreen());
+    final themeMode = ref.watch(themeNotifierProvider);
+    return DevicePreview(
+      backgroundColor: Colors.white,
+      enabled: true,
+      availableLocales: const [
+        Locale('en', 'US'),
+      ],
+      devices: [
+        Devices.macOS.macBookPro,
+        Devices.linux.laptop,
+        Devices.android.samsungGalaxyA50,
+        Devices.android.samsungGalaxyS20,
+        Devices.ios.iPhone13ProMax,
+        Devices.ios.iPhoneSE,
+        Devices.windows.laptop,
+      ],
+      tools: const [
+        DeviceSection(
+          model: true,
+          orientation: false,
+          frameVisibility: false,
+          virtualKeyboard: false,
+        )
+      ],
+      builder: (context) => MaterialApp(
+          useInheritedMediaQuery: true,
+          locale: DevicePreview.locale(context),
+          builder: DevicePreview.appBuilder,
+          debugShowCheckedModeBanner: false,
+          theme: theme,
+          darkTheme: darkTheme,
+          themeMode: themeMode,
+          home: const TabsScreen()),
+    );
   }
 }
